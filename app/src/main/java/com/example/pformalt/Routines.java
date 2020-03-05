eb84309e8a41f298ac38773c93687a15472167c2
tarnaldo88 <tarnaldo88@gmail.com>
2020-03-04 20:44:54 -0800

    changes made to routine page Torres

--- app/src/main/java/com/example/pformalt/Routines.java
+++ app/src/main/java/com/example/pformalt/Routines.java
@@ -1,6 +1,7 @@
 package com.example.pformalt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,47 +16,45 @@ public class Routines extends AppCompatActivity {
 
     private ImageButton BtnMoveBack;
     private ImageButton BtnRotateBody;
+    private ImageButton rotateToFront;
+    private ImageButton clearBtn;
     private Button chest;
     private Button legs;
     private Button arms;
+    private Button shoulder;
+    private Button backPart;
     private ImageView chestSelected;
     private ImageView armSelected;
     private ImageView legSelected;
-
-    ArrayList<String> selectedParts;
-    //ArrayList<String> selectedParts = bundleParts.getStringArrayList("list");
-
-    /*
-    To do:
-    Make an array that stores the values of which body part is selected
-    use bundle to move array back and forth between activities as user selects parts
-    utilize a method that at start of activity displays the values of array that have been accessed
-     */
-
+    private ImageView backSelect;
+    private ImageView shoulderSelected;
+    private ImageView bg;
+    private ArrayList<String> selectedParts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routines);
-        //list to store what areas user selects
 
-        Bundle bundleParts = new Bundle();
-
+        bg = findViewById(R.id.background);
+        clearBtn = findViewById(R.id.clear);
         BtnMoveBack = findViewById(R.id.backMainActivityBtn);
         BtnRotateBody = findViewById(R.id.RotateBtn);
+        rotateToFront = findViewById(R.id.rotateFromBack);
         chest = findViewById(R.id.chestBtn);
         legs = findViewById(R.id.legBtn);
         arms = findViewById(R.id.armBtn);
-
+        shoulder = findViewById(R.id.shoulderBtn);
+        backPart = findViewById(R.id.backBodyPt);
 
         //Image views that pop up when user selects a body part
         chestSelected =(ImageView) findViewById(R.id.chestSelected);
         armSelected =(ImageView) findViewById(R.id.armSelected);
         legSelected =(ImageView) findViewById(R.id.legSelected);
+        backSelect =(ImageView) findViewById(R.id.backSelected);
+        shoulderSelected =(ImageView) findViewById(R.id.shoulderSelected);
 
         selectedParts = new ArrayList<String>();
-        selectedParts.add("test");
-
 
         //listener to activate button upon click by user, selects chest
         chest.setOnClickListener(new View.OnClickListener() {
@@ -93,6 +92,30 @@ public class Routines extends AppCompatActivity {
             }
         });
 
+        //listener to activate button upon click by user, selects chest
+        shoulder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //button activity goes in here, check that user has not already selected area
+                if(!selectedParts.contains("shoulder")) {
+                    selectedParts.add("shoulder");
+                }
+                shoulderSelected.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //listener to activate button upon click by user, selects arms
+        backPart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //button activity goes in here, check that user has not already selected area
+                if(!selectedParts.contains("back")) {
+                    selectedParts.add("back");
+                }
+                backSelect.setVisibility(View.VISIBLE);
+            }
+        });
+
         //listener to activate button upon click by user, goes back to main page
         BtnMoveBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,11 +124,27 @@ public class Routines extends AppCompatActivity {
             }
         });
 
-        //listener to activate button upon click by user, goes back to main page
+        //listener to activate button upon click by user, turns body around
         BtnRotateBody.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 moveToBackSideBody();
+            }
+        });
+
+        //listener to activate button upon click by user, turns body around
+        rotateToFront.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                moveToFrontSideBody();
+            }
+        });
+
+        //listener to activate button upon click by user, clears list
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                clearAll(selectedParts);
             }
         });
     }
@@ -116,10 +155,36 @@ public class Routines extends AppCompatActivity {
         startActivity(goToMainActivity);;
     }
 
+    private void clearAll(ArrayList x){
+        x.clear();
+        shoulderSelected.setVisibility(View.INVISIBLE);
+        legSelected.setVisibility(View.INVISIBLE);
+        armSelected.setVisibility(View.INVISIBLE);
+        chestSelected.setVisibility(View.INVISIBLE);
+        backSelect.setVisibility(View.INVISIBLE);
+    }
+
     //function to move to Routines Activity back view
     private void moveToBackSideBody(){
-        Intent goToBackSide = new Intent(Routines.this, RoutinesBackSide.class);
+        //change chest arm and leg button invisible
+        legs.setVisibility(View.GONE);
+        chest.setVisibility(View.GONE);
+        arms.setVisibility(View.GONE);
+        shoulder.setVisibility(View.VISIBLE);
+        backPart.setVisibility(View.VISIBLE);
+        rotateToFront.setVisibility(View.VISIBLE);
+        BtnRotateBody.setVisibility(View.GONE);
+        bg.setImageDrawable(ContextCompat.getDrawable(Routines.this, R.drawable.routine_back));
+    }
 
-        startActivity(goToBackSide);
+    //function to rotate back to front facing body
+    private void moveToFrontSideBody(){
+        legs.setVisibility(View.VISIBLE);
+        chest.setVisibility(View.VISIBLE);
+        arms.setVisibility(View.VISIBLE);
+        shoulder.setVisibility(View.GONE);
+        backPart.setVisibility(View.GONE);
+        BtnRotateBody.setVisibility(View.VISIBLE);
+        bg.setImageDrawable(ContextCompat.getDrawable(Routines.this, R.drawable.bg_routines));
     }
 }
