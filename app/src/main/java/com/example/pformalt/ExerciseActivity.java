cf6b68561e9e33589709390ca1cea054f8f988f3
Emmanoel Dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-03-24 17:22:51 -0700

    implemented youtube api

--- app/src/main/java/com/example/pformalt/ExerciseActivity.java
+++ app/src/main/java/com/example/pformalt/ExerciseActivity.java
@@ -5,27 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
-public class ExerciseActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-    private ImageButton backToBodyGroups;
+public class ExerciseActivity extends YouTubeBaseActivity { //AppCompatActivity,
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise);
+        private ImageButton backToBodyGroups;
+        Button button;
+        private YouTubePlayerView youtubePlayerView;
+        private YouTubePlayer.OnInitializedListener onInitializedListener;
 
-        backToBodyGroups = findViewById(R.id.backMainActivityBtn);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_exercise);
 
-        //listener to activate button upon click by user, selects chest
-        backToBodyGroups.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                moveToBody();
-            }
-        });
-    }
+            youtubePlayerView = findViewById(R.id.youtube_view);
+            backToBodyGroups = findViewById(R.id.backMainActivityBtn);
+
+            onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+                @Override
+                public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                    youTubePlayer.loadVideo( "lXwm62SiLQ8"); //youtube video link
+
+                }
+
+                @Override
+                public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+                }
+            };
+
+            button = findViewById(R.id.button);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    youtubePlayerView.initialize("AIzaSyC8GktotpkFtqSWHVUGQjXBg4UVHD52qf0", onInitializedListener); //API key
+                }
+            });
+            //listener to activate button upon click by user, selects chest
+            backToBodyGroups.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    moveToBody();
+                }
+            });
+        }
 
     private void moveToBody(){
         Intent goToBodyActivity = new Intent(ExerciseActivity.this, BodyGroups.class);
