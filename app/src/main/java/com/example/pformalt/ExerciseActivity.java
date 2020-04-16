87f79f921f590ad7c42c21f5f066d71d79a01445
emmanoel dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-04-16 02:18:36 -0700

    made it so that the program reads input from a csv file

--- app/src/main/java/com/example/pformalt/ExerciseActivity.java
+++ app/src/main/java/com/example/pformalt/ExerciseActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +16,14 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExerciseActivity extends YouTubeBaseActivity { //I removed the extends AppCompatActivity idk if it was needed but I couldn't extend two at once
 
         private ImageButton backToBodyGroups;
@@ -23,7 +32,6 @@ public class ExerciseActivity extends YouTubeBaseActivity { //I removed the exte
         private YouTubePlayer.OnInitializedListener onInitializedListener;
         TextView textView;
 
-
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -31,10 +39,15 @@ public class ExerciseActivity extends YouTubeBaseActivity { //I removed the exte
 
             textView = (TextView) findViewById(R.id.description);
 
+            readWorkoutData();
+
+            //final String YTLink = searchYTL(workoutSamples, 0);
+            String description = workoutSamples.get(0).getDescription();
+
             /*
             * IGNORE THE WEIRD TEXT IT WAS JUST TO TEST THE SCROLLING FEATURE
             */
-            String description = "  Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
+            /*String description = "  Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
@@ -42,14 +55,14 @@ public class ExerciseActivity extends YouTubeBaseActivity { //I removed the exte
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
-                    "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
+                    "Exercise is any bossdily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\n" +
                     "Exercise is any bodily activity that enhances or maintains physical fitness and overall health and wellness.\nTHIS IS THE LAST THING";
-
+            */
             textView.setText(description);
             textView.setMovementMethod(new ScrollingMovementMethod());
 
@@ -62,7 +75,7 @@ public class ExerciseActivity extends YouTubeBaseActivity { //I removed the exte
             onInitializedListener = new YouTubePlayer.OnInitializedListener() {
                 @Override
                 public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                    youTubePlayer.loadVideo( "lXwm62SiLQ8"); //youtube video link
+                    youTubePlayer.loadVideo(workoutSamples.get(0).getYTLink()); //youtube video link lXwm62SiLQ8 /searchYTL(workoutSamples, 0)
                 }
 
                 @Override
@@ -90,4 +103,53 @@ public class ExerciseActivity extends YouTubeBaseActivity { //I removed the exte
         Intent goToBodyActivity = new Intent(ExerciseActivity.this, BodyGroups.class);
         startActivity(goToBodyActivity);
     }
+
+    private List<workoutSample> workoutSamples = new ArrayList<>();
+    private void readWorkoutData() {
+        //workoutSample sample;
+        InputStream is = getResources().openRawResource(R.raw.workoutdata);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+
+        try {
+            //using this to step over the headers
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                //split by ,
+                String[] tokens = line.split(",");
+
+                //read the data
+                workoutSample sample = new workoutSample();
+                sample.setId(Integer.parseInt(tokens[0]));
+                sample.setWName(tokens[1]);
+                sample.setYTLink(tokens[2]);
+                sample.setDescription(tokens[3]);
+                workoutSamples.add(sample);
+            }
+        } catch (IOException e) {
+            Log.wtf("ExerciseActivity", "Error reading csv file on line " + line, e);
+            e.printStackTrace();
+        }
+    }
+    public static String searchYTL (List<workoutSample> workouts, int id) {
+        for (int i = 0; i < workouts.size(); i++) {
+            if (workouts.get(i).getId() == id) {
+                return workouts.get(i).getYTLink();
+            }
+        }
+        return ""; //no link found
+    }
+    public static String searchDesc (List<workoutSample> workouts, int id) {
+        for (int i = 0; i < workouts.size(); i++) {
+            if (workouts.get(i).getId() == id) {
+                return workouts.get(i).getDescription();
+            }
+        }
+        return "Description not found.";
+    }
+
 }
+
---
+++ app/src/main/java/com/example/pformalt/workoutSample.java
@@ -0,0 +1,42 @@
+package com.example.pformalt;
+
+import java.util.List;
+
+class workoutSample {
+    private int id;
+    private String wName;
+    private String ytLink;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWName() {
+        return wName;
+    }
+
+    public void setWName(String wName) {
+        this.wName = wName;
+    }
+
+    public String getYTLink() {
+        return ytLink;
+    }
+
+    public void setYTLink(String ytLink) {
+        this.ytLink = ytLink;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
